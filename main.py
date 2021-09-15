368b75caf73094f7bc31edec61133db730d8585a
Barinov <barinovgm@gmail.com>
2021-09-15 17:06:04 +0300

    pep-8

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from app.md5_create import md5_create
 import json
 
 with open("countries.json") as f:
-  countries = json.load(f)
+    countries = json.load(f)
 
 if __name__ == "__main__":
-  get_links(countries)
-  md5_create("country_with_wikilink.json")
+    get_links(countries)
+    md5_create("country_with_wikilink.json")
